75ef0f0c890c6a0dd54259a867e5207d00140900
Kousuke Seikai <seikai@sonicmoov.com>
2016-08-18 03:11:40 +0900

    bugfix

--- fluent-plugin-google-cloud-storage.gemspec
+++ fluent-plugin-google-cloud-storage.gemspec
@@ -4,12 +4,12 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 Gem::Specification.new do |gem|
   gem.name          = "fluent-plugin-google-cloud-storage"
-  gem.version       = "1.0.1"
+  gem.version       = "1.0.2"
   gem.authors       = ["Hsiu-Fan Wang"]
   gem.email         = ["hfwang@porkbuns.net"]
   gem.summary       = %q{Fluentd plugin to write data to Google Cloud Storage}
   gem.description   = %q{Google Cloud Storage fluentd output}
-  gem.homepage      = "https://github.com/hfwang/fluent-plugin-google-cloud-storage"
+  gem.homepage      = "https://github.com/sonicmoov/fluent-plugin-google-cloud-storage"
   gem.license       = "APLv2"
 
   gem.files         = `git ls-files`.split($\)
@@ -24,6 +24,8 @@ Gem::Specification.new do |gem|
   gem.add_runtime_dependency "fluentd", "~> 0.12.0"
   gem.add_runtime_dependency "fluent-mixin-plaintextformatter", '>= 0.2.1'
   gem.add_runtime_dependency "fluent-mixin-config-placeholders", ">= 0.3.0"
-  gem.add_runtime_dependency 'google-api-client', '> 0.9'
+  gem.add_runtime_dependency "google-api-client", "~> 0.9.3"
   gem.add_runtime_dependency "mime-types", '>= 3.0'
 end
+
+
--- lib/fluent/plugin/out_google_cloud_storage.rb
+++ lib/fluent/plugin/out_google_cloud_storage.rb
@@ -3,44 +3,47 @@
 require 'fluent/mixin/config_placeholders'
 require 'fluent/mixin/plaintextformatter'
 require 'fluent/log'
+require 'uri'
 
 class Fluent::GoogleCloudStorageOutput < Fluent::TimeSlicedOutput
-    
+
   Fluent::Plugin.register_output('google_cloud_storage', self)
-  
+
   include Fluent::Mixin::ConfigPlaceholders
-    
+
   include Fluent::Mixin::PlainTextFormatter
-  
+
   config_set_default :buffer_type, 'file'
   config_set_default :time_slice_format, '%Y%m%d%H'
   
+  config_param :ignore_start_check_error, :bool, :default => false
+
   # Available methods are:
   # * private_key -- Use service account credential from pkcs12 private key file
   # * compute_engine -- Use access token available in instances of ComputeEngine
   # * private_json_key -- Use service account credential from JSON key
   # * application_default -- Use application default credential
   config_param :auth_method, :string, default: 'private_key'
-  
+
   ### Service Account credential
   config_param :email, :string, default: nil
   config_param :private_key_path, :string, default: nil
   config_param :private_key_passphrase, :string, default: 'notasecret', secret: true
   config_param :json_key, default: nil
-  
+
   ### Save File Params
   config_param :bucket_id, :string
   config_param :path, :string
 
   config_param :compress, :default => nil do |val|
-      unless ["gz", "gzip"].include?(val)
-        raise ConfigError, "Unsupported compression algorithm '#{val}'"
-      end
-      val
+    unless ["gz", "gzip"].include?(val)
+      raise ConfigError, "Unsupported compression algorithm '#{val}'"
+    end
+    val
   end
 
   config_param :default_tag, :string, :default => 'tag_missing'
-  
+
   CHUNK_ID_PLACE_HOLDER = '${chunk_id}'
 
   def initialize
@@ -51,6 +54,7 @@ class Fluent::GoogleCloudStorageOutput < Fluent::TimeSlicedOutput
     require 'mime-types'
     require 'googleauth'
     require 'google/apis/storage_v1'
+    @cached_client_expiration = 0
   end
 
   # Define `log` method for v0.10.42 or earlier
@@ -59,7 +63,7 @@ class Fluent::GoogleCloudStorageOutput < Fluent::TimeSlicedOutput
   end
 
   def configure(conf)
-      
+
     if conf['path']
       if conf['path'].index('%S')
         conf['time_slice_format'] = '%Y%m%d%H%M%S'
@@ -73,74 +77,70 @@ class Fluent::GoogleCloudStorageOutput < Fluent::TimeSlicedOutput
     super
 
     case @auth_method
-    when 'private_key'
-      unless @email && @private_key_path
-        raise Fluent::ConfigError, "'email' and 'private_key_path' must be specified if auth_method == 'private_key'"
-      end
-    when 'compute_engine'
-      # Do nothing
-    when 'json_key'
-      unless @json_key
-        raise Fluent::ConfigError, "'json_key' must be specified if auth_method == 'json_key'"
-      end
-    when 'application_default'
-      # Do nothing
-    else
-      raise Fluent::ConfigError, "unrecognized 'auth_method': #{@auth_method}"
+      when 'private_key'
+        unless @email && @private_key_path
+          raise Fluent::ConfigError, "'email' and 'private_key_path' must be specified if auth_method == 'private_key'"
+        end
+      when 'compute_engine'
+        # Do nothing
+      when 'json_key'
+        unless @json_key
+          raise Fluent::ConfigError, "'json_key' must be specified if auth_method == 'json_key'"
+        end
+      when 'application_default'
+        # Do nothing
+      else
+        raise Fluent::ConfigError, "unrecognized 'auth_method': #{@auth_method}"
     end
 
     if @path.index(CHUNK_ID_PLACE_HOLDER).nil?
       raise Fluent::ConfigError, "path must contain ${chunk_id}, which is the placeholder for chunk_id, when append is set to false."
     end
   end
-  
+
   def client
-      
+
     return @client if @client && @cached_client_expiration > Time.now
 
-    client = Google::Apis::StorageV1::StorageService.new.tap do |cl|
-      cl.authorization = get_auth
-    end
-
-    @cached_client_expiration = Time.now + 1800
-    @client = client
-    
-  end
-
-  def prepare_client
-      
-      @auth_option = {
-        private_key_path: @private_key_path, 
+    @auth_options = {
+        private_key_path: @private_key_path,
         private_key_passphrase: @private_key_passphrase,
         email: @email,
         json_key: @json_key,
-      }
+    }
+
+    @scope = [
+        Google::Apis::StorageV1::AUTH_CLOUD_PLATFORM,
+        Google::Apis::StorageV1::AUTH_DEVSTORAGE_FULL_CONTROL
+    ]
     
-      @scope  = [
-          Google::Apis::StorageV1::AUTH_CLOUD_PLATFORM_READ_ONLY, 
-          Google::Apis::StorageV1::AUTH_DEVSTORAGE_FULL_CONTROL
-      ]
-      
-      client
-      
+    client = Google::Apis::StorageV1::StorageService.new.tap do |c|
+      c.authorization = get_auth
+      c.authorization.fetch_access_token!
+    end
+
+    @cached_client_expiration = Time.now + 1800
+
+    @client = client
+
   end
-  
+
   #
   # auth for api client
   # @see https://github.com/kaizenplatform/fluent-plugin-bigquery/blob/master/lib/fluent/plugin/bigquery/writer.rb
   #
   def get_auth
     case @auth_method
-    when 'private_key'
-      get_auth_from_private_key
-    when 'compute_engine'
-      get_auth_from_compute_engine
-    when 'json_key'
-      get_auth_from_json_key
-    when 'application_default'
-      get_auth_from_application_default
-    else
-      raise ConfigError, "Unknown auth method: #{@auth_method}"
+      when 'private_key'
+        get_auth_from_private_key
+      when 'compute_engine'
+        get_auth_from_compute_engine
+      when 'json_key'
+        get_auth_from_json_key
+      when 'application_default'
+        get_auth_from_application_default
+      else
+        raise ConfigError, "Unknown auth method: #{@auth_method}"
     end
   end
 
@@ -152,11 +152,11 @@ class Fluent::GoogleCloudStorageOutput < Fluent::TimeSlicedOutput
 
     key = Google::APIClient::KeyUtils.load_from_pkcs12(private_key_path, private_key_passphrase)
     Signet::OAuth2::Client.new(
-      token_credential_uri: "https://accounts.google.com/o/oauth2/token",
-      audience: "https://accounts.google.com/o/oauth2/token",
-      scope: @scope,
-      issuer: email,
-      signing_key: key
+        token_credential_uri: "https://accounts.google.com/o/oauth2/token",
+        audience: "https://accounts.google.com/o/oauth2/token",
+        scope: @scope,
+        issuer: email,
+        signing_key: key
     )
   end
 
@@ -180,7 +180,7 @@ class Fluent::GoogleCloudStorageOutput < Fluent::TimeSlicedOutput
   def get_auth_from_application_default
     Google::Auth.get_application_default([@scope])
   end
-  
+
   def start
     super
   end
@@ -191,33 +191,56 @@ class Fluent::GoogleCloudStorageOutput < Fluent::TimeSlicedOutput
 
   def path_format(chunk_key)
     path = Time.strptime(chunk_key, @time_slice_format).strftime(@path)
-    log.debug "GCS Path: #{path}"
     path
   end
 
   def chunk_unique_id_to_str(unique_id)
-    unique_id.unpack('C*').map{|x| x.to_s(16).rjust(2,'0')}.join('')
+    unique_id.unpack('C*').map { |x| x.to_s(16).rjust(2, '0') }.join('')
   end
   
+  def _write(path, io, encoding)
+    
+    begin
+      
+      client.insert_object(@bucket_id, Google::Apis::StorageV1::Object.new, 
+        name: URI.escape(path), 
+        content_encoding: encoding, 
+        upload_source: io, 
+        content_type: "application/json") do |res, err|
+        if err
+          log.warn err
+          @client = nil
+        end
+      end
+
+    rescue Google::Apis::ServerError, Google::Apis::ClientError, Google::Apis::AuthorizationError => e
+      @client = nil
+      message = e.message
+      log.warn "storage.get API", code: e.status_code, message: message, path: path
+    end
+    
+  end
 
   def write(chunk)
-     
-     path = path_format(chunk.key).gsub(CHUNK_ID_PLACE_HOLDER, chunk_unique_id_to_str(chunk.unique_id))
-     
-     mimetype = MIME::Types.type_for(path).first
+    
+    data = chunk.read
+    
+    path = path_format(chunk.key).gsub(CHUNK_ID_PLACE_HOLDER, chunk_unique_id_to_str(chunk.unique_id))
 
-     io = nil
-     if ["gz", "gzip"].include?(@compress)
-       io = StringIO.new("")
-       writer = Zlib::GzipWriter.new(io)
-       writer.write(chunk.read)
-       writer.finish
-       io.rewind
-     else
-       io = StringIO.new(data)
-     end
-     
-     storage.insert_object(@bucket_name, {name: path}, upload_source: io, content_type:mimetype.content_type)
-
+    io = nil
+    encoding = nil
+    if ["gz", "gzip"].include?(@compress)
+      io = StringIO.new("")
+      writer = Zlib::GzipWriter.new(io)
+      writer.write(data)
+      writer.finish
+      io.rewind
+      encoding = "gzip"
+    else
+      io = StringIO.new(data)
+    end
+    
+    _write(path, io, encoding)
+    
   end
 end
